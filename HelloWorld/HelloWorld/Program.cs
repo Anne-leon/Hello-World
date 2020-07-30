ac22e6d3df3f3522cca70038f0783065aba31328
Anne-leon <68597838+Anne-leon@users.noreply.github.com>
2020-07-30 13:55:29 +0200

    Add files via upload

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World");
+			Console.ReadLine();
+		}
+	}
+}
