cf47467dc36af21ee5557a350d2ed6d72f4e2764
Anne-leon <68597838+Anne-leon@users.noreply.github.com>
2020-08-10 17:48:26 +0200

    Add files via upload

---
+++ Defining and Calling Methods/Defining and Calling Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Defining_and_Calling_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helloworld();
+            Console.ReadLine();
+        }
+
+        private static void Helloworld()
+        {
+            Console.WriteLine("Hello world!"!);
+        }
+
+
+
+    }
+}
